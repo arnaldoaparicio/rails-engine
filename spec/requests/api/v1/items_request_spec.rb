b55ca849645805aef1de9b27efa55f69de009d9e
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-02-08 16:00:37 -0500

    Initial commit

---
+++ spec/requests/api/v1/items_request_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'Item API' do
+  it 'sends a list of merchants' do
+    merchant = create(:merchant)
+    create_list(:item, 3, merchant: merchant)
+
+
+    get '/api/v1/items'
+
+    expect(response).to be_successful
+    items = JSON.parse(response.body, symbolize_names: true)
+    expect(items.count).to eq(3)
+  end
+end
