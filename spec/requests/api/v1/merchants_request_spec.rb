8534360cdc1c7c918a567174ad3fdd5cb9406d88
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-02-08 18:47:35 -0500

    Refactor tests to match serializer

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -9,33 +9,31 @@ describe 'Merchant API' do
     expect(response).to be_successful
 
     merchants = JSON.parse(response.body, symbolize_names: true)
-    expect(merchants.count).to eq(3)
 
-    merchants.each do |merchant|
+    
+    expect(merchants[:data].count).to eq(3)
+    expect(merchants[:data][0]).to have_key(:id)
+    merchants[:data].each do |merchant|
       expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_an(Integer)
-      
-      expect(merchant).to have_key(:name)
-      expect(merchant[:name]).to be_a(String)
+      expect(merchant[:id]).to be_an(String)
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_a(String)
     end
   end
 
-  it 'can get one merchant by its id' do 
-    id = create(:merchant).id 
+  it 'can get one merchant by its id' do
+    id = create(:merchant).id
 
     get "/api/v1/merchants/#{id}"
 
     merchant = JSON.parse(response.body, symbolize_names: true)
+    # binding.pry
+    expect(response).to be_successful
 
+    expect(merchant[:data]).to have_key(:id)
+    expect(merchant[:data][:id]).to eq(id.to_s)
 
-    expect(response).to be_successful 
-
-
-    expect(merchant).to have_key(:id)
-    expect(merchant[:id]).to eq(id)
-
-    expect(merchant).to have_key(:name)
-    expect(merchant[:name]).to be_a(String)
-
+    expect(merchant[:data][:attributes]).to have_key(:name)
+    expect(merchant[:data][:attributes][:name]).to be_a(String)
   end
 end
