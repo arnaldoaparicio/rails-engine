eb11220e7e6919da4fa3ae0fdb71e4fd599ff0a3
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-02-10 16:46:36 -0500

    Add routes for finding merchant

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/api/v1/merchants/find', to: 'api/v1/merchant_search#show'
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index, :show]
-      resources :items, only: [:index, :show]
+      resources :merchants, only: [:index, :show] do
+        resources :items
+      end
+      resources :items, only: [:index, :show, :create, :update, :destroy]
     end
   end
+
+  get '/api/v1/items/:item_id/merchant', to: 'api/v1/merchants#index'
 end
