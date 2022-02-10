87d9b5f625fe9dbc78721ca233f32c3757a84e79
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-02-10 16:40:19 -0500

    Refactor all methods

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,9 +1,23 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
-    render json: MerchantSerializer.new(Merchant.all)
+    if params.has_key?(:item_id)
+      item = Item.find(params[:item_id])
+      json_response(MerchantSerializer.new(item.merchant))
+    else
+      render json: MerchantSerializer.new(Merchant.all)
+    end
   end
 
   def show
     render json: MerchantSerializer.new(Merchant.find(params[:id]))
+  end
+
+
+
+
+  private
+
+  def merchant_params
+    params.permit(:name)
   end
 end
