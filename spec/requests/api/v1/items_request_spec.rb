5049cd6a45b431efe4ac53f52e3ce06116f50cd0
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-02-10 19:54:28 -0500

    Add more tests for deleting nonexistant item and item update

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -31,6 +31,7 @@ describe 'Item API' do
 
     item = JSON.parse(response.body, symbolize_names: true)
     expect(response).to be_successful
+    expect(response.status).to eq(200)
 
     expect(item[:data]).to have_key(:id)
 
@@ -55,6 +56,7 @@ describe 'Item API' do
     created_item = Item.last
 
     expect(response).to be_successful
+    expect(response.status).to eq(201)
     expect(created_item.name).to eq(item_params[:name])
     expect(created_item.description).to eq(item_params[:description])
     expect(created_item.unit_price).to eq(item_params[:unit_price])
@@ -70,21 +72,23 @@ describe 'Item API' do
     delete "/api/v1/items/#{item.id}"
 
     expect(response).to be_successful
+    expect(response.status).to eq(204)
     expect(Item.count).to eq(0)
-    expect{Item.find(item.id)}.to raise_error(ActiveRecord::RecordNotFound)
+    expect { Item.find(item.id) }.to raise_error(ActiveRecord::RecordNotFound)
   end
 
   it 'can update an existing item' do
     merchant = create(:merchant)
     id = create(:item, merchant: merchant).id
     previous_name = Item.last.name
-    item_params = { name: 'Electric Boogaloo 2'}
-    headers = {'CONTENT_TYPE' => 'application/json'}
+    item_params = { name: 'Electric Boogaloo 2' }
+    headers = { 'CONTENT_TYPE' => 'application/json' }
 
-    patch "/api/v1/items/#{id}", headers: headers, params: JSON.generate({item: item_params})
+    patch "/api/v1/items/#{id}", headers: headers, params: JSON.generate({ item: item_params })
     item = Item.find_by(id: id)
 
     expect(response).to be_successful
+    expect(response.status).to eq(200)
     expect(item.name).to_not eq(previous_name)
     expect(item.name).to eq('Electric Boogaloo 2')
   end
@@ -92,13 +96,39 @@ describe 'Item API' do
   it 'updates an existing item without merchant id' do
     merchant = create(:merchant)
     id = create(:item, merchant: merchant).id
-    item_params = { merchant_id: ''}
-    headers = {'CONTENT_TYPE' => 'application/json'}
+    item_params = { merchant_id: '' }
+    headers = { 'CONTENT_TYPE' => 'application/json' }
 
-    patch "/api/v1/items/#{id}", headers: headers, params: JSON.generate({item: item_params})
+    patch "/api/v1/items/#{id}", headers: headers, params: JSON.generate({ item: item_params })
     item = Item.find_by(id: id)
 
-
     expect(response.status).to eq(400)
+  end
+
+  it 'fails to create an item' do
+    merchant = create(:merchant)
+    item_params = {
+      name: 'Thousand-Yard Sword',
+      description: 'Lost sword supposedly belonging to Ganon.',
+      merchant_id: merchant
+    }
+
+    headers = { 'CONTENT_TYPE' => 'application/json' }
+    post '/api/v1/items', headers: headers, params: JSON.generate(item: item_params)
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
+  end
+
+  it 'deletes a nonexistant item' do
+    merchant = create(:merchant)
+    item = create(:item, merchant: merchant)
+
+    expect(Item.count).to eq(1)
+
+    delete "/api/v1/items/#{item.id + 1}"
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(404)
   end
 end
