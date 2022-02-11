3a2dec4619e72d4f673cd20d1879462ebac21c56
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-02-11 08:55:22 -0500

    Add index to show all merchants in a search

--- app/controllers/api/v1/merchant_search_controller.rb
+++ app/controllers/api/v1/merchant_search_controller.rb
@@ -1,13 +1,25 @@
 class Api::V1::MerchantSearchController < ApplicationController
-
+  def index
+    results = Merchant.all_merchants_search(params[:name])
+    if params[:name].nil?
+      render json: { data: { message: 'Search parameters cannot be missing' } }, status: 400
+    elsif results.empty?
+      render json: MerchantSerializer.new(results), status: 400
+    elsif params[:name] == ''
+      render json: { data: { message: 'Search cannot be empty' } }, status: 400
+    else
+      json_response(MerchantSerializer.new(results))
+    end
+  end
+  
   def show
     results = Merchant.merchant_search(params[:name])
     if params[:name].nil?
-      render json: {data: {message: 'Search parameters cannot be missing'}}, status: 400
+      render json: { data: { message: 'Search parameters cannot be missing' } }, status: 400
     elsif results.empty?
       render json: { data: { message: 'No results found' } }, status: 400
     elsif params[:name] == ''
-      render json: {data: {message: 'Search cannot be empty'}}, status: 400
+      render json: { data: { message: 'Search cannot be empty' } }, status: 400
     else
       json_response(MerchantSerializer.new(results.first))
     end
