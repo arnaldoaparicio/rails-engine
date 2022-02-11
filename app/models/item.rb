fd3cdd850a60dd93b1a661c79e61cf980c0ab39b
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-02-11 08:56:26 -0500

    Add item_search method

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :merchant
 
-
+  has_many :invoice_items, dependent: :destroy
   validates_presence_of :merchant_id
+
+  def self.all_items_search(items)
+    where('name ILIKE ?', "%#{items}%").order(:name)
+  end
+
+  def self.item_search(item)
+    where('name ILIKE ?', "%#{item}%").order(:name).limit(1)
+  end
 end
