c7d010353c7e9fdbb5d26890bcbb531fc914513e
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-02-15 15:36:45 -0500

    WIP for item search

--- app/controllers/api/v1/item_search_controller.rb
+++ app/controllers/api/v1/item_search_controller.rb
@@ -13,13 +13,23 @@ class Api::V1::ItemSearchController < ApplicationController
   end
 
   def show
+
     results = Item.item_search(params[:name])
-    if params[:name].nil?
+    if params.has_key?(:name) && params[:name].nil?
       render json: { data: { message: 'Search parameters cannot be missing' } }, status: 400
-    elsif results.empty?
-      render json: { data: { message: 'No results found' } }, status: 400
     elsif params[:name] == ''
       render json: { data: { message: 'Search cannot be empty' } }, status: 400
+    elsif params.has_key?(:min_price)
+      min = Item.minimum_price(params[:min_price])
+      json_response(ItemSerializer.new(min))
+    elsif params.has_key?(:max_price)
+      max = Item.maximum_price(params[:max_price])
+      json_response(ItemSerializer.new(max))
+    elsif params.has_key?(:max_price) && params.has_key?(:min_price)
+      min_and_max = Item.min_and_max_price(params[:max_price], params[:min_price])
+      json_response(ItemSerializer.new(min_and_max))
+    elsif params.empty?
+      render json: { data: { message: 'No results found' } }, status: 400
     else
       json_response(ItemSerializer.new(results.first))
     end
